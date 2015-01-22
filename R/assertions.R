c87cacff97e1b1fbbe4f3c2d422d37b0d91d2410
Tony Fischetti <tony.fischetti@gmail.com>
2015-01-21 23:02:00 -0500

    added 'verify' verb... testing it now
    also changed things around with assert

--- R/assertions.R
+++ R/assertions.R
@@ -21,7 +21,7 @@
 
 
 #############################################################################
-### assert_vector(data.frame, column_from_data.frame, predicate_function) ###
+### assert(data.frame, column_from_data.frame, predicate_function) ###
 #############################################################################
 # uses predicate provided on every element of a vector
 # and either errors out (with helpful message)
@@ -44,13 +44,14 @@
 #' not_na("tree")
 #'
 #' @export
-assert_vector <- function(the.frame, column, predicate){
-  name.of.column <- stringify(substitute(column))
+assert <- function(the.frame, column, predicate){
+  #assert <- function(data, attrbs, predicate,
+  name.of.column <- deparse(substitute(column))
   if(!(name.of.column %in% names(the.frame))){
     stop(paste0("column '", name.of.column, "' not in data.frame '",
                 stringify(substitute(the.frame)), "'"))
   }
-  name.of.predicate <- get.name.of.function(stringify(substitute(predicate)))
+  name.of.predicate <- get.name.of.function(deparse(substitute(predicate)))
   the.vector <- the.frame[[name.of.column]]
   full.predicate <- make.predicate.proper(predicate)
   log.vect <- vapply(the.vector, full.predicate, logical(1))
@@ -67,7 +68,68 @@ assert_vector <- function(the.frame, column, predicate){
 # REWRITE THIS SO IT STOPS AT THE FIRST VIOLATION
 
 
+
+
+
+
+#' Raises error if expression is FALSE anywhere
+#'
+#' Meant for use in a data analysis pipeline, this function will
+#' just return the data it's supplied if all the logicals in the
+#' expression supplied are TRUE. If at least one is FALSE, this
+#' function will raise a error, effectively terminating the pipeline
+#' early
+#'
+#' @param data A data frame, list, or environment
+#' @param expr A logical expression
+#' @param character.only a logical indicating whether attributes
+#'        from \code{data} in \code{expr} can be assumed to be
+#'        character strings (default FALSE)
+#'
+#' @return data if verification passes. errors if not
+#' @note See \code{vignette("assertr")} for how to use this in context
+#' @seealso \code{\link{assert}}
+#' @examples
+#'
+#' verify(mtcars, drat > 2)     # returns mtcars
+#' verify(mtcars, drat > 3)     # produces error
+#'
+#' library(magrittr)            # for piping operator
+#'
+#' mtcars %>%
+#'   verify(drat > 3) %>%
+#'   # anything here will not run
+#'
+#' mtcars %>%
+#'  verify(nrow(mtcars)>2)
+#'  # nothing here will run
+#'
+#' alist <- list(a=c(1,2,3), b=c(4,5,6))
+#' verify(alist, length(a) > 2)
+#' verify(alist, length(a) > 2 && length(b) > 2)
+#' verify(alist, a > 0 & b > 2)
+#'
+#' alist %>%
+#'   verify(alist, length(a) > 5)
+#'   # nothing here will run
+#'
+#'
 #' @export
-assert <- assert_vector
+verify <- function(data, expr, character.only=FALSE){
+  if(!character.only)
+    expr <- substitute(expr)
+  # conform to terminology from subset
+  envir <- data
+  enclos <- parent.frame()
+  logical.results <- eval(expr, envir, enclos)
+  if(all(logical.results))
+    return(data)
+  stop("verification failed!")
+}
+
+
+
+
+
 
 
--- tests/testthat/test-assertions.R
+++ tests/testthat/test-assertions.R
@@ -1,70 +1,25 @@
 context("assertions about assertions in assertion.R")
 
 
-############### not_na ###############
-test_that("not_na returns true if not NA", {
-  expect_equal(not_na("tree"), TRUE)
-  expect_equal(not_na(2.8),    TRUE)
-  expect_equal(not_na(8),      TRUE)
+
+
+############### verify ###############
+test_that("verify returns data if verification passes", {
+
+  # just some set up
+  a <- 1
+  alist <- list(a=c(1,2,3), b=c(4,5,6))
+
+  expect_equal(verify(mtcars, drat > 2), mtcars)
+  expect_equal(verify(mtcars, nrow(mtcars) > 2), mtcars)
+  expect_equal(verify(mtcars, am %in% c(0,1)), mtcars)
+  expect_equal(verify(mtcars, a > 0), mtcars)
+  expect_equal(verify(alist, length(a) > 0), alist)
+  # respects scoping rules?
+  expect_equal(verify(alist, length(a) > 2), alist)
+  expect_equal(verify(alist, length(a) > 2 && length(b) > 2), alist)
+  expect_equal(verify(alist, a >= 1 & b > 2), alist)
+
 })
 
-test_that("not_na returns FALSE if NA", {
-  expect_equal(not_na(NA), FALSE)
-})
-
-test_that("not_na handles NaNs right", {
-  expect_equal(not_na(NaN), FALSE)
-  expect_equal(not_na(NaN, allow.NaN=FALSE), FALSE)
-  expect_equal(not_na(NaN, allow.NaN=TRUE), TRUE)
-})
-
-test_that("not_na errors out when appropriate", {
-  expect_error(not_na(c(1,2)),       "not_na must be called with single element")
-  expect_error(not_na(diag(c(1,2))), "not_na must be called with single element")
-  expect_error(not_na(c()),          "not_na must be called with single element")
-  expect_error(not_na(),             "argument \"x\" is missing, with no default")
-})
 ######################################
-
-
-#####################################
-########### within_bounds ###########
-
-test_that("within_bounds fails appropriately", {
-  expect_error(within_bounds(), "argument \"lower.bound\" is missing, with no default")
-  expect_error(within_bounds(1, "tree"), "bounds must be numeric")
-  expect_error(within_bounds(2, 1), "lower bound must be strictly lower than upper bound")
-  expect_error(within_bounds(2, 2), "lower bound must be strictly lower than upper bound")
-})
-
-test_that("returned predicate works appropriately", {
-  expect_equal(within_bounds(3, 4)(pi), TRUE)
-  expect_equal(within_bounds(3, 4)(3), TRUE)
-  expect_equal(within_bounds(3, 4, include.lower=FALSE)(3), FALSE)
-  expect_equal(within_bounds(3, 4)(4), TRUE)
-  expect_equal(within_bounds(3, 4, include.upper=FALSE)(4), FALSE)
-  expect_equal(within_bounds(3, 4)(10), FALSE)
-  expect_equal(within_bounds(3, 4)(as.numeric(NA)), TRUE)
-  expect_equal(within_bounds(3, 4, allow.NA=FALSE)(as.numeric(NA)), FALSE)
-  expect_equal(within_bounds(0, Inf)(0), TRUE)
-  expect_equal(within_bounds(0, Inf, include.lower=FALSE)(0), FALSE)
-  expect_equal(within_bounds(0, Inf)(10), TRUE)
-  expect_equal(within_bounds(0, Inf)(Inf), TRUE)
-  expect_equal(within_bounds(0, Inf, include.upper=FALSE)(Inf), FALSE)
-})
-
-test_that("returned predicate fails appropriately", {
-  expect_error(within_bounds(0,1)(), "argument \"x\" is missing, with no default")
-  expect_error(within_bounds(0,1)("tree"), "bounds must only be checked on numerics")
-  expect_error(within_bounds(0,1)(c(1,2)), "bounds must be checked on a single element")
-  expect_error(within_bounds(0,1)(c()), "bounds must be checked on a single element")
-
-})
-
-
-# if(length(x)>1)      stop("bounds must be checked on a single element")
-# if(is.null(x))       stop("bounds must be checked on a single element")
-# if(!is.numeric(x))   stop("bounds must only be checked on numerics")
-
-
-#####################################
