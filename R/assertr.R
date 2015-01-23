3b3b87ecfd81b1b953976ee99774a276f368822b
Tony Fischetti <tony.fischetti@gmail.com>
2015-01-23 13:38:53 -0500

    everything checks out. Just have to write README

--- R/assertr.R
+++ R/assertr.R
@@ -6,16 +6,25 @@
 #' See the assertr vignette or the documentation for more information \cr
 #' > \code{vignette("assertr")}
 #'
+#' You may also want to read the documentation for the function that
+#' \code{assertr} provides:
+#' \itemize{
+#'   \item \code{\link{assert}}
+#'   \item \code{\link{verify}}
+#'   \item \code{\link{not_na}}
+#'   \item \code{\link{in_set}}
+#'   \item \code{\link{within_bounds}}
+#'   }
+#'
 #'
 #' @examples
-#' \dontrun{
 #' library(magrittr)     # for the piping operator
 #'
 #' mtcars %>%
 #'   verify(mpg > 0) %>%
 #'   assert(in_set(0,1), am, vs) %>%
-#'   assert(is.numeric, mpg:carb) %>%
-#'   # do something actually interesting here}
+#'   assert(is.numeric, mpg:carb)
+#'   # do something actually interesting here
 #'
 #'
 #' @docType package
